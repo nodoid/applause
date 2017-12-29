22b3fc4c38cddf1d78a666854e6f663146ba0cb9
Paul Johnson <paul@snuberry-software.co.uk>
2017-12-29 12:38:28 +0000

    Initial import

---
+++ applause/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace applause
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::applause.Properties.Resources.applause2;
+            this.button1.Location = new System.Drawing.Point(29, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 235);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 309);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Applause please!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ applause/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using System.Resources;
+
+namespace applause
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isplaying = false;
+
+        private SoundPlayer player = new SoundPlayer(applause.Properties.Resources.Crowd_Applaud_Cheer_13215_Free_Loops_com);
+
+        public void playaudio()
+        {
+                player.Play();
+                isplaying = true;
+        }
+
+        public void stopaudio()
+        {
+            if (isplaying == true)
+            {
+                player.Stop();
+                isplaying = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isplaying == false)
+            {
+                button1.Image = applause.Properties.Resources.applause1;
+                playaudio();
+            }
+            else
+            {
+                button1.Image = applause.Properties.Resources.applause2;
+                stopaudio();
+            }
+        }
+
+
+    }
+}
